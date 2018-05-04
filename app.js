8580e95c9d1a17a240ddff9684a09dbdb3c73216
Jakub Butkiewicz <jakubbutkiewicz@MacBook-Air-Jakub.local>
2018-05-04 16:48:18 +0200

    change app js

--- app.js
+++ app.js
@@ -7,16 +7,16 @@ function getWiki(e) {
     },1000);
     
     let word = document.querySelector('#search').value;
-    let url = `https://en.wikipedia.org/w/api.php?format=json&action=query&generator=search&gsrnamespace=0&gsrlimit=10&prop=pageimages|extracts&pilimit=max&exintro&explaintext&exsentences=1&exlimit=max&gsrsearch=${word}&callback=JSON_CALLBACK`;
+    let url = `https://en.wikipedia.org/w/api.php?format=jsonp&action=query&generator=search&gsrnamespace=0&gsrlimit=10&prop=pageimages|extracts&pilimit=max&exintro&explaintext&exsentences=1&exlimit=max&gsrsearch=${word}&callback=JSON_CALLBACK`;
+    const xhr = new XMLHttpRequest();
 
-    fetch(url, {
-        mode: 'no-cors'
-    }).then(function(response){
-        return response.json();
-    }).then(function(data){
-        console.log(data);
-    }) 
-    
+    xhr.open('GET',url,true);
+    xhr.onload = function() {
+        if(this.status == 200) {
+            console.log(JSON.parse(this.responseText));
+        }
+    }
+    xhr.send();
 
 }
 
