e1c78bcc83781676b51689dee2b1e5840e9350c2
Jakub Butkiewicz <jakubbutkiewicz@MacBook-Air-Jakub.local>
2018-05-05 00:40:48 +0200

    change app js

--- app.js
+++ app.js
@@ -1,22 +1,37 @@
 const searchBtn = document.getElementById('search-btn');
+
+function UI(data) {
+    const list = document.querySelector('.list');
+    output = [];
+    for (let i = 0; i < data.query.pages.length; i++) {
+        output += `
+    <a href="${data.query.pages[i].canonicalurl}" target="_blank">
+        <li class="list-item">
+            <p>${data.query.pages[i].title}</p>
+        </li>
+    </a>
+    `;
+    }
+    list.innerHTML = output;
+}
+
 function getWiki(e) {
     e.preventDefault();
-    searchBtn.classList.add('animated','flipInX');
+    searchBtn.classList.add('animated', 'flipInX');
     setTimeout(() => {
-        searchBtn.classList.remove('animated','flipInX');
-    },1000);
-    
-    let word = document.querySelector('#search').value;
-    let url = `https://en.wikipedia.org/w/api.php?format=jsonp&action=query&generator=search&gsrnamespace=0&gsrlimit=10&prop=pageimages|extracts&pilimit=max&exintro&explaintext&exsentences=1&exlimit=max&gsrsearch=${word}&callback=JSON_CALLBACK`;
-    const xhr = new XMLHttpRequest();
+        searchBtn.classList.remove('animated', 'flipInX');
+    }, 1000);
 
-    xhr.open('GET',url,true);
-    xhr.onload = function() {
-        if(this.status == 200) {
-            console.log(JSON.parse(this.responseText));
-        }
-    }
-    xhr.send();
+    let word = document.querySelector('#search').value;
+    let url = `https://en.wikipedia.org/w/api.php?action=query&format=json&gsrlimit=15&generator=search&formatversion=2&origin=*&gsrsearch=${word}&exlimit=5&prop=info|extracts&inprop=url`;
+
+    fetch(url).then(function (response) {
+        return response.json();
+    }).then(function (data) {
+        console.log(data);
+        UI(data);
+    })
+
 
 }
 
